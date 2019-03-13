5691ed4840be0c75a8cf8b0fcaa9a7c5854a8fb3
Ean Daus <eandaus123@gmail.com>
2019-03-13 19:24:39 -0400

    added signup page and live JS validation feedback

---
+++ scripts/signUpVal.js
@@ -0,0 +1,30 @@
+/*
+* Ean Daus
+* 3/13/19
+* signUpVal.js
+* Event handlers for inline form validation in signUp.html
+*/
+
+//when the password field gains focus
+$("#pass").on("focus", function(){
+    //clear the message box
+    $("#passMessage").html( '<p class="text-primary">Between 8 and 15 characters, must contain upper and lowercase letters, and numbers.</p>');
+});
+
+//when the password field loses focus
+$("#pass").on("blur", function(){
+    //get the password, define regex
+    let pass = $("#pass").val();
+    let regex = RegExp('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$', 'g');
+
+    //test the password against regex
+    if(regex.test(pass))
+    {
+        //display success message
+        $("#passMessage").html( '<p class="text-success">Between 8 and 15 characters, must contain upper and lowercase letters, and numbers.</p>');
+    }else
+    {
+        //display error message
+        $("#passMessage").html( '<p class="text-danger">Between 8 and 15 characters, must contain upper and lowercase letters, and numbers.</p>');
+    }
+});
